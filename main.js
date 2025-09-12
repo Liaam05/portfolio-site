c83e5e8c0a63a7d783c0165d84802f7733e5f639
市川凌大 <ichikawaryohta@MacBook-Air.local>
2025-09-12 16:59:01 +0900

    add changeImg

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+let myButton = document.querySelector('button');
+
+
+function chengeImg() {
+    let myImage = document.querySelector('img');
+    let mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/first_img.png") {
+        myImage.setAttribute("src", "images/second_img.png");
+    } else {
+        myImage.setAttribute("src", "images/first_img.png");
+    }
+}
+
+myButton.onclick = chengeImg;
+
+/*
 const myImage = document.querySelector('img');
 
 myImage.onclick = () => {
@@ -8,3 +24,4 @@ myImage.onclick = () => {
         myImage.setAttribute("src", "images/first_img.png");
     }
 }
+*/
